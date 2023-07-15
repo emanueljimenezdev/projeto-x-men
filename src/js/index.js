8f74beec7f286a42541ad9d640ff961c5ec3b93f
Emanuel Jiménez <123333295+emanueljimenezdev@users.noreply.github.com>
2023-07-15 10:10:34 -0300

    Edição Final e Refaturação do Codigo

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,40 @@
-const personagens = document.querySelectorAll('.personagem')
-personagens.forEach((personagem) =>{
+const personagens = document.querySelectorAll('.personagem');
+personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        const personagemSelecionado = document.querySelector('.selecionado')
-        personagemSelecionado.classList.remove('selecionado')
-        personagem.classList.add('selecionado')
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `.src/imagens/card-${idPersonagem}.png`;
+        if(window.innerWidth , 450) {
+            window.scrollTo({top:0, behavior: 'smooth' });
+        }
+
+        removerSelecaoDoPersonagem();
+        
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
     })
 })
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
