5cdf36248ffaffac386a5c6e8b8682ea0423f3d1
Emanuel Jiménez <123333295+emanueljimenezdev@users.noreply.github.com>
2023-07-15 08:58:30 -0300

    JS part2

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 const personagens = document.querySelectorAll('.personagem')
 personagens.forEach((personagem) =>{
     personagem.addEventListener('mouseenter', () => {
-        personagem.classList.add('selecionado')
         const personagemSelecionado = document.querySelector('.selecionado')
         personagemSelecionado.classList.remove('selecionado')
+        personagem.classList.add('selecionado')
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `.src/imagens/card-${idPersonagem}.png`;
     })
 })
